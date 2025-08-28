3784ddc853864361ad8604cc820a8a5a9fa96786
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2025-08-28 19:18:37 -0400

    Adding final tests

--- libReflectionHelpers/src/test/java/redactedrice/reflectionhelpers/objects/ExtendedObjectTests.java
+++ libReflectionHelpers/src/test/java/redactedrice/reflectionhelpers/objects/ExtendedObjectTests.java
@@ -6,7 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -74,6 +77,55 @@ public class ExtendedObjectTests {
             eo.attrMap.put(FIELD, EXPECTED);
             assertEquals(EXPECTED, eo.eoGet(FIELD));
             assertEquals(EXPECTED, eo.eoGet(FIELD, DEFAULT));
+        }
+    }
+
+    @Test
+    void eoSet() {
+        final String EXPECTED = "expected";
+        final String EXPECTED2 = "expectedToo";
+        final String FIELD = "field";
+
+        SimpleObject so = new SimpleObject("test obj", 3);
+        ExtendableObject eo = spy(ExtendableObject.create(so));
+        
+        doReturn(true).when(eo).setInternal(any(), any());
+        eo.eoSet(FIELD, EXPECTED);
+        assertTrue(eo.attrMap.isEmpty());
+        assertTrue(eo.eoSetIfExists(FIELD, EXPECTED));
+        assertTrue(eo.attrMap.isEmpty());
+
+        doReturn(false).when(eo).setInternal(any(), any());
+        assertFalse(eo.eoSetIfExists(FIELD, EXPECTED));
+        assertTrue(eo.attrMap.isEmpty());
+        eo.eoSet(FIELD, EXPECTED);
+        assertEquals(1, eo.attrMap.size());
+        assertEquals(EXPECTED, eo.attrMap.get(FIELD));
+
+        assertTrue(eo.eoSetIfExists(FIELD, EXPECTED2));
+        assertEquals(1, eo.attrMap.size());
+        assertEquals(EXPECTED2, eo.attrMap.get(FIELD));
+        
+    }
+    
+    @Test
+    void setInternal() {
+        final String EXPECTED = "expected";
+        final String FIELD = "field";
+
+        SimpleObject so = new SimpleObject("test obj", 3);
+        ExtendableObject eo = spy(ExtendableObject.create(so));
+
+        try (MockedStatic<ReflectionUtils> mocked = mockStatic(ReflectionUtils.class)) {
+            mocked.when(() -> ReflectionUtils.setWithSetter(any(), any(), any())).thenAnswer(invocation -> null);
+            assertTrue(eo.setInternal(FIELD, EXPECTED));
+
+            mocked.when(() -> ReflectionUtils.setWithSetter(any(), any(), any())).thenThrow(new NoSuchMethodException());
+            mocked.when(() -> ReflectionUtils.setWithField(any(), any(), any())).thenAnswer(invocation -> null);
+            assertTrue(eo.setInternal(FIELD, EXPECTED));
+
+            mocked.when(() -> ReflectionUtils.setWithField(any(), any(), any())).thenThrow(new NoSuchFieldException());
+            assertFalse(eo.setInternal(FIELD, EXPECTED));
         }
     }
 }
